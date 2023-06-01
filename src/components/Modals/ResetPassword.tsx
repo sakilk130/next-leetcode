6ffe0fbbe875674e027c322de3249ba6bb9aa3cf
sakilk130 <sakilk130@gmail.com>
2023-06-02 00:30:15 +0600

    feat: reset password

--- src/components/Modals/ResetPassword.tsx
+++ src/components/Modals/ResetPassword.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSendPasswordResetEmail } from 'react-firebase-hooks/auth';
+import { toast } from 'react-hot-toast';
+
+import { auth } from '@/config/firebase';
+
+import { LoadingCircle } from '../Icons';
 
 type ResetPasswordProps = {};
 
 const ResetPassword: React.FC<ResetPasswordProps> = () => {
+  const [email, setEmail] = useState<string>('');
+  const [sendPasswordResetEmail, sending, error] =
+    useSendPasswordResetEmail(auth);
+
+  const handleReset = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const success = await sendPasswordResetEmail(email);
+    if (success) {
+      toast.success('Password reset email sent');
+    }
+  };
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error.message);
+    }
+  }, [error]);
+
   return (
-    <form className="px-6 pb-4 space-y-6 lg:px-8 sm:pb-6 xl:pb-8">
+    <form
+      className="px-6 pb-4 space-y-6 lg:px-8 sm:pb-6 xl:pb-8"
+      onSubmit={handleReset}
+    >
       <h3 className="text-xl font-medium text-white">Reset Password</h3>
       <p className="text-sm text-white ">
         Forgotten your password? Enter your e-mail address below, and we&apos;ll
@@ -18,6 +45,8 @@ const ResetPassword: React.FC<ResetPasswordProps> = () => {
           Your email
         </label>
         <input
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
           type="email"
           name="email"
           id="email"
@@ -27,9 +56,13 @@ const ResetPassword: React.FC<ResetPasswordProps> = () => {
       </div>
       <button
         type="submit"
-        className="w-full text-white  focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-brand-orange hover:bg-brand-orange-s"
+        className="w-full text-white  focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-brand-orange hover:bg-brand-orange-s disabled:opacity-50 disabled:cursor-progress"
+        disabled={sending}
       >
-        Reset Password
+        <span className="flex items-center justify-center">
+          {sending && <LoadingCircle />}
+          Reset Password
+        </span>
       </button>
     </form>
   );
