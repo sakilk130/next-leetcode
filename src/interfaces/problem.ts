962d9d44232fa85df4f8a5e452de9e558e0f4ff2
sakilk130 <sakilk130@gmail.com>
2023-06-04 23:32:24 +0600

    feat: import all problems description

---
+++ src/interfaces/problem.ts
@@ -0,0 +1,31 @@
+export interface Example {
+  id: number;
+  inputText: string;
+  outputText: string;
+  explanation?: string;
+  img?: string;
+}
+
+export interface Problem {
+  id: string;
+  title: string;
+  problemStatement: string;
+  examples: Example[];
+  constraints: string;
+  order: number;
+  starterCode: string;
+  handlerFunction: ((fn: any) => boolean) | string;
+  starterFunctionName: string;
+}
+
+export interface DBProblem {
+  id: string;
+  title: string;
+  category: string;
+  difficulty: string;
+  likes: number;
+  dislikes: number;
+  order: number;
+  videoId?: string;
+  link?: string;
+}
